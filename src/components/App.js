0279822b26c6b029cac202b67ee003a2d63962ed
kuldeepparmar <kuldeep07122002@gmail.com>
2025-07-04 00:19:26 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,65 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Step from "./Step";
 
 const App = () => {
+
+const [step,setStep] = useState(0);
+  const handleprev=()=>{
+    setStep(step-1);
+  }
+  const handlenext=()=>{
+    setStep(step+1);
+  }
+
+  const handlesubmit = (event)=>{
+    event.preventDefault();
+    setStep(0);
+    
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        
+        {
+              step===0 && <Step
+                id1={"first_name"}
+                id2={"last_name"}
+                name1={"firstname"}
+                name2={"lastname"}
+                label1={"First Name:"}
+                label2={"Last Name:"}
+                handlenext={handlenext}
+              />
+        }
+        {
+            step===1 && <Step
+            id1={"model"}
+            id2={"car_price"}
+            name1={"model"}
+            name2={"carprice"}
+            label1={"Model:"}
+            label2={"Price:"}
+            handlenext={handlenext}
+            handleprev={handleprev}
+            />
+        }
+        {
+           step===2 &&  <Step
+           
+           id1={"card_info"}
+           id2={"expiry_date"}
+           name1={"cardnumber"}
+           name2={"exipryDate"}
+           label1={"Credit Card Number:"}
+           label2={"Expiry Date:"}
+           handleprev={handleprev}
+            handlesubmit={handlesubmit}
+           />
+          
+        }
     </div>
+
   )
 }
 
---
+++ src/components/Step.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Step = (props) => {
+    const{id1,id2,handleprev,handlenext,label1,label2,name1,name2,handlesubmit}  = props;
+    
+  return (
+    <div>
+  <form onSubmit={handlesubmit}>
+    <label for={id1}>{label1}</label>
+    <br></br>
+    <input id={id1} name={name1}></input>
+    <br></br>
+    <label for={id2}>{label2}</label>
+    <br></br>
+    <input id={id2} name={name2}></input>
+    <br></br>
+    {
+        handleprev && (<button onClick={handleprev}>Prev</button>)
+    }
+    
+    {
+        handlenext && (<button onClick={handlenext}>Next</button>)
+    }
+    
+    {
+       handlesubmit && <button type='submit'>submit</button>
+    }
+  </form>
+
+    </div>
+  )
+}
+
+export default Step
